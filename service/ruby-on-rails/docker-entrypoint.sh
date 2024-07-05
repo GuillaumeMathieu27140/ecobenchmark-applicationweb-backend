3cf6c4efa3da8ddadd1d89635f4b9940446861f4
PGMU01201 <ext.guillaume.mathieu@sncf.fr>
2024-07-05 11:44:50 +0200

    Docker image rootless

--- service/ruby-on-rails/docker-entrypoint.sh
+++ service/ruby-on-rails/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/workdir/bin/rails db:migrate RAILS_ENV=development
+/home/ror/bin/rails db:migrate RAILS_ENV=development
 
-/workdir/bin/rails server -b 0.0.0.0
+/home/ror/bin/rails server -b 0.0.0.0
